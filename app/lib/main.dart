112f8a096355ac6a5f860ab0f089e39f278f57a2
Jagger Denhof <denhofja@mail.gvsu.edu>
2022-10-05 22:39:43 -0400

    OUT-23: Hello world App up

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'OOTD',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,12 +19,12 @@ class MyApp extends StatelessWidget {
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // or simply save your changes to s"hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Outfit Of The Day'),
     );
   }
 }
