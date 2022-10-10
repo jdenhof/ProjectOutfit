7a14eefb65351a35e91a4bb0dcd09ad94d014f3a
Jagger Denhof <denhofja@mail.gvsu.edu>
2022-10-10 15:02:32 -0400

    OUT-26: Flip camera function works

--- src/lib/components/camera_page/camera.dart
+++ src/lib/components/camera_page/camera.dart
@@ -3,43 +3,129 @@ import 'package:flutter/material.dart';
 import 'package:app/main.dart';
 
 class CameraScreen extends StatefulWidget {
-  const CameraScreen({
-    super.key,
-  });
-
+  const CameraScreen({super.key});
   @override
   State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _cameraController;
+class _CameraScreenState extends State<CameraScreen>
+    with WidgetsBindingObserver {
+  CameraController? _cameraController;
+  bool _isCameraInitialized = false;
+  bool _isRearCameraSelected = true;
+
+  void _newCameraSelected(CameraDescription camera) async {
+    final prevCameraController = _cameraController;
+
+    final CameraController newCameraController = CameraController(
+      camera,
+      ResolutionPreset.high,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    await prevCameraController?.dispose();
+
+    if (mounted) {
+      setState(() {
+        _cameraController = newCameraController;
+      });
+    }
+
+    newCameraController.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    try {
+      await newCameraController.initialize();
+    } on CameraException catch (e) {
+      throw ('Error initializing camera: $e');
+    }
+
+    if (mounted) {
+      setState(() {
+        _isCameraInitialized = _cameraController!.value.isInitialized;
+      });
+    }
+  }
+
+  void _flipCamera() {}
+
+  void _takePicture() {}
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = _cameraController;
+
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      _newCameraSelected(cameraController.description);
+    }
+  }
 
   @override
   void initState() {
+    _isRearCameraSelected = true;
+    _newCameraSelected(cameras[_isRearCameraSelected ? 0 : 1]);
     super.initState();
-    _cameraController = CameraController(cameras[0], ResolutionPreset.max);
-    _cameraController.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    });
   }
 
   @override
   void dispose() {
     // Dispose of the controller when the widget is disposed.
-    _cameraController.dispose();
+    _cameraController?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: _cameraController.value.isInitialized
-          ? CameraPreview(_cameraController)
-          : const Center(child: CircularProgressIndicator()),
-    ));
+      body: _isCameraInitialized
+          ? Container(
+              child: AspectRatio(
+              aspectRatio: 1 / _cameraController!.value.aspectRatio,
+              child: _cameraController!.buildPreview(),
+            ))
+          : Container(),
+      floatingActionButton: Row(
+        children: [
+          //Flip Camera Icon
+          InkWell(
+            onTap: () {
+              setState(() {
+                _isCameraInitialized = false;
+              });
+              _newCameraSelected(
+                cameras[_isRearCameraSelected ? 1 : 0],
+              );
+              setState(() {
+                _isRearCameraSelected = !_isRearCameraSelected;
+              });
+            },
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                const Icon(
+                  Icons.circle,
+                  color: Colors.black38,
+                  size: 60,
+                ),
+                Icon(
+                  _isRearCameraSelected
+                      ? Icons.camera_front
+                      : Icons.camera_rear,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
